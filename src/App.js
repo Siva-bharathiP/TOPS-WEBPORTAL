f6d50faeba175cf9e3ab6954c647c582a7832245
Siva-bharathiP <136304492+Siva-bharathiP@users.noreply.github.com>
2024-02-09 14:28:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
 import Greet from './component/Function';
-import Form from './component/form';
+// import Form from './component/form';
 import Prop from './component/Props/props';
 // import defaultPropcompo from './component/Props/defaultpropcompo';
 import DefaultPropcompo from './component/Props/defaultProp';
 import ChildProps from './component/Props/childProps';
+import Form from './component/Regexform';
 function App() {
   return (
     <div className="App">
       {/* <Greet /> */}
-      <Form /> 
+      {/* <Form />  */}
       {/* <Prop name="Props type"/> */}
       {/* <DefaultPropcompo/>
       <ChildProps>
       <p>Hi Everyone</p>
       <p>Hi Everyone</p>
       </ChildProps> */}
+      <Form/>
     </div>
   );
 }
